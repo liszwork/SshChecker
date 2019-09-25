cd5768ea84416a7d0329ffdf5bcf08ba49661841
lisns <z.z.iz.0pnup@gmail.com>
2019-09-25 14:53:46 +0900

    ベースの作成

---
+++ SshChecker/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace SshChecker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if ( disposing && (components != null) )
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxCommand = new System.Windows.Forms.TextBox();
+            this.textBoxResult = new System.Windows.Forms.TextBox();
+            this.buttonRun = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxCommand
+            // 
+            this.textBoxCommand.Location = new System.Drawing.Point(12, 30);
+            this.textBoxCommand.Name = "textBoxCommand";
+            this.textBoxCommand.Size = new System.Drawing.Size(394, 19);
+            this.textBoxCommand.TabIndex = 0;
+            // 
+            // textBoxResult
+            // 
+            this.textBoxResult.Location = new System.Drawing.Point(12, 84);
+            this.textBoxResult.Multiline = true;
+            this.textBoxResult.Name = "textBoxResult";
+            this.textBoxResult.Size = new System.Drawing.Size(394, 226);
+            this.textBoxResult.TabIndex = 0;
+            // 
+            // buttonRun
+            // 
+            this.buttonRun.Location = new System.Drawing.Point(331, 55);
+            this.buttonRun.Name = "buttonRun";
+            this.buttonRun.Size = new System.Drawing.Size(75, 23);
+            this.buttonRun.TabIndex = 1;
+            this.buttonRun.Text = "run";
+            this.buttonRun.UseVisualStyleBackColor = true;
+            this.buttonRun.Click += new System.EventHandler(this.buttonRun_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "  ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "return";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "command";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(422, 328);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonRun);
+            this.Controls.Add(this.textBoxResult);
+            this.Controls.Add(this.textBoxCommand);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxCommand;
+        private System.Windows.Forms.TextBox textBoxResult;
+        private System.Windows.Forms.Button buttonRun;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ SshChecker/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Renci.SshNet;
+
+namespace SshChecker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// SSHコマンド処理
+        /// </summary>
+        /// <param name="command">コマンド</param>
+        /// <returns>標準出力</returns>
+        private string[] SshCmd(string command, int sshTimeoutSec = -1)
+        {
+            SshClient ssh = new SshClient("192.168.0.100", "user", "pass");
+            ssh.Connect();
+            string[] str_result;
+            string str_results;
+            char ptn = ',';
+
+            int timeout = (sshTimeoutSec <= -1) ? 60 : sshTimeoutSec;
+            // 60秒タイムアウト付きでコマンド実行
+            var result = ssh.RunCommand("timeout -sKILL " + timeout + " " + command);
+            str_results = result.Result;
+            str_results = str_results.Trim('\n');
+            str_result = str_results.Split(ptn);
+            ssh.Disconnect();
+
+            if ( str_result.Length > 0 && str_result[0].Equals("") )
+            {
+                throw new TimeoutException("SSHコマンドがタイムアウトしました");
+            }
+
+            return str_result;
+        }
+
+        /// <summary>
+        /// 実行ボタン クリックイベント
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonRun_Click(object sender, EventArgs e)
+        {
+            string cmd = textBoxCommand.Text;
+            string[] str_result = SshCmd(cmd);
+            textBoxResult.Text = "";
+            foreach (var item in str_result)
+            {
+                textBoxResult.Text += "- " + item + "\r\n";
+            }
+        }
+    }
+}
